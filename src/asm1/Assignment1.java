fd65ebed517b44a74695515277a2c48e2e142af0
tunv <51073921+kimochich@users.noreply.github.com>
2023-03-28 15:55:45 +0700

    update

--- src/asm1/Assignment1.java
+++ src/asm1/Assignment1.java
@@ -4,6 +4,14 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Assignment1 {
+
+    //  MODE_HARD = true tạo chuỗi 6 số bảo mật ngẫu nhiên
+    //  false tạo tạo chuỗi 3 số bảo mật từ 100->999
+    final static boolean MODE_HARD = false;
+    final static String PROJECT_NAME = "NGAN HANG SO";
+    final static String STUDENT_CODE = "xM02173";
+    final static String VERSION = "v1.0.0";
+
     public static void main(String[] args) {
         menu();
     }
@@ -11,7 +19,7 @@ public class Assignment1 {
     public static void menu() {
         Scanner sc = new Scanner(System.in);
         System.out.println("+-----+-------------------+------+");
-        System.out.println("| NGAN HANG SO | xM02173@v1.0.0  |");
+        System.out.println("| "+PROJECT_NAME+" | "+STUDENT_CODE+"@"+VERSION+"  |");
         System.out.println("+-----+-------------------+------+");
         System.out.println("| 1. Nhap CCCD                   |");
         System.out.println("| 0. Thoat                       |");
@@ -49,18 +57,33 @@ public class Assignment1 {
         System.exit(0);
     }
 
+    public static String randomKey() {
+        if (MODE_HARD) {
+            Random random = new Random();
+            // khai báo mảng ký tự có thể chứa trong mã bảo mật
+            char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+            StringBuilder maBaoMat = new StringBuilder();
+            for (int i = 0; i < 6; i++) {
+                int index = random.nextInt(chars.length);
+                maBaoMat.append(chars[index]);
+            }
+            return maBaoMat.toString();
+        } else {
+            return String.valueOf((new Random()).nextInt(900) + 100);
+        }
+    }
+
     private static void inputCCCD(Scanner sc) {
         boolean isError;
-        // tạo số random từ 100 - 999
-        int radomNum = (new Random()).nextInt(900) + 100;
-        System.out.println("Nhap ma xac thuc: " + radomNum);
+        String randomKey = randomKey();
+        System.out.println("Nhap ma xac thuc: " + randomKey);
         do {
             try {
-                int code = Integer.parseInt(sc.nextLine());
+                String code = sc.nextLine();
                 // lặp lại cho đến khi nhập đúng mã xác thực
-                while (code != radomNum) {
+                while (!code.equals(randomKey)) {
                     System.out.println("Ma xac thuc khong dung.Vui long nhap lai");
-                    code = Integer.parseInt(sc.nextLine());
+                    code = sc.nextLine();
                 }
                 System.out.println("Nhap vao CCCD");
                 String cccd = sc.nextLine();
@@ -73,7 +96,7 @@ public class Assignment1 {
                     }
                 }
                 // hiển thị menu kiểm tra số CCCD
-                menuCCCD(sc, cccd);
+                menuChild(sc, cccd);
                 isError = false;
             } catch (Exception e) {
                 System.out.println("Ma xac thuc khong dung.Vui long nhap lai");
@@ -82,7 +105,7 @@ public class Assignment1 {
         } while (isError); // lặp lại cho đến khi số nhập vào hợp lệ
     }
 
-    private static void menuCCCD(Scanner sc, String cccd) {
+    private static void menuChild(Scanner sc, String cccd) {
         boolean willContinue = true;
         do {
             System.out.println("| 1. Kiem tra noi sinh                   |");
@@ -93,19 +116,22 @@ public class Assignment1 {
             try {
                 System.out.print("Chuc nang: ");
                 n = Integer.parseInt(sc.nextLine());
-                while (n < 0 || n > 1) {
+                while (n < 0 || n > 3) {
                     System.out.println("Khong hop le, vui long nhap lai");
                     n = sc.nextInt();
                 }
                 switch (n) {
                     case 1:
+                        //truyền vào mã tỉnh là 3 chữ số đầu
                         checkPlaceOfBirth(cccd.substring(0, 3));
                         break;
                     case 2:
-                        checkAge(cccd);
+                        // truyền vào mã giới tính và mã năm sinh
+                        checkAge(Integer.parseInt(cccd.substring(3, 4)), Integer.parseInt(cccd.substring(4, 6)));
                         break;
                     case 3:
-                        checkDigit(cccd);
+                        //truyền vào 6 số ngẫu nhiên cuối dùng
+                        checkDigit(cccd.substring(6, 12));
                         break;
                     case 0:
                         willContinue = false;
@@ -119,12 +145,31 @@ public class Assignment1 {
 
     }
 
-    private static void checkDigit(String cccd) {
-
+    private static void checkDigit(String code) {
+        System.out.println("So ngau nhien la: " + code);
     }
 
-    private static void checkAge(String cccd) {
-
+    private static void checkAge(int gender, int year) {
+        int centuryYear = 1900;
+        switch (gender) {
+            case 2:
+            case 3:
+                centuryYear = 2000;
+                break;
+            case 4:
+            case 5:
+                centuryYear = 2100;
+                break;
+            case 6:
+            case 7:
+                centuryYear = 2200;
+                break;
+            case 8:
+            case 9:
+                centuryYear = 2300;
+                break;
+        }
+        System.out.println("Gioi tinh: " + (gender % 2 == 0 ? "Nam " : "Nu ") + "| " + (centuryYear + year));
     }
 
     private static void checkPlaceOfBirth(String code) {
@@ -148,7 +193,14 @@ public class Assignment1 {
             case "010":
                 province = "Lao Cai";
                 break;
+            case "011":
+                province = "Dien Bien";
+                break;
+            default:
+                province = "Khong xac dinh";
+                break;
         }
+        System.out.println("Noi sinh: " + province);
     }
 
 
